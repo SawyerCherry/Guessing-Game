de6ac9a6368cb21d2f000406d90e7c74e2c6fce0
Sawyer Cherry <sawyercherry@macbook-pro-78.local>
2018-08-20 08:48:21 -0500

    Initial Commit

---
+++ The Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  The Guessing Game
+//
+//  Created by Sawyer Cherry on 8/20/18.
+//  Copyright © 2018 Sawyer Cherry. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
