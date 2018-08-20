3653ce8764046a890d8ce49ec3a41a28799bc1f6
Sawyer Cherry <sawyercherry@macbook-pro-78.local>
2018-08-20 12:51:20 -0500

    Guessing Game

--- The Guessing Game/main.swift
+++ The Guessing Game/main.swift
@@ -6,7 +6,76 @@
 //  Copyright © 2018 Sawyer Cherry. All rights reserved.
 //
 
+
+
 import Foundation
 
-print("Hello, World!")
+
+var Repeat = ""
+repeat {
+    
+    print("Guess the Number I'm thinking between 1 and 100!")
+    let randomNumber = Int(arc4random_uniform(99) + 1)
+    var numberOfTries = 10
+    var guessedNumber: Int? = nil
+    
+    repeat {
+        
+        repeat {
+            var input = readLine()!
+            if input == "Give Me Da Wae" {
+                print("Da Wae is \(randomNumber)")
+            }
+            input =  readLine()!
+        
+            if let number = Int(input) {
+                if number >= 1 && number <= 100 {
+                    guessedNumber = number
+                } else {
+                    print("Please Enter A Valid Number!")
+                    guessedNumber = nil
+                }
+            } else {
+                print("Please Enter A Valid Number!")
+                guessedNumber = nil
+            }
+            
+        } while guessedNumber == nil
+        
+        guard let guessedNumber = guessedNumber else {
+            fatalError()
+        }
+        
+        if guessedNumber == randomNumber {
+            print("Your guess is correct, You Win! Cookies and Cakes for all the mates!")
+        }
+        
+        if guessedNumber > randomNumber {
+            print("Your Guess Was Too High!")
+        }
+        if guessedNumber < randomNumber {
+            print("Your Guess Was Too Low")
+        }
+        numberOfTries -= 1
+        print("You have \(numberOfTries) tries left")
+        if guessedNumber != randomNumber {
+            print("Please guess another number!")
+        }
+    } while numberOfTries > 0 && guessedNumber! != randomNumber
+    
+    print(" You now have the Option to Quit or Restart!")
+    Repeat = readLine()!
+    
+    
+} while Repeat.lowercased() == "yes"
+
+
+
+
+
+
+
+
+
+
 
